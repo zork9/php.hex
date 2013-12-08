2e665219b2f003d4ed71850491195d8e53d7cc8c
zork9 <bubble.rogue@gmail.com>
2013-12-08 19:59:19 +0100

    hex dump

--- src/file.php
+++ src/file.php
@@ -35,58 +35,58 @@ function printHexArrayToFile($array)
 	for ($i = 0; $i < length($array); $i++) {
 		switch ($array[$i]) {
 			case '0':
-				fwrite($fp, hex2bin("0000"));
+				fwrite($fp, hex2bin("0"));
 				break;
 			case '1':	
-				fwrite($fp, hex2bin("0001"));
+				fwrite($fp, hex2bin("1"));
 				break;
 			case '2':	
-				fwrite($fp, hex2bin("0010"));
+				fwrite($fp, hex2bin("2"));
 				break;
 			case '3':	
-				fwrite($fp, hex2bin("0011"));
+				fwrite($fp, hex2bin("3"));
 				break;
 			case '4':	
-				fwrite($fp, hex2bin("0100"));
+				fwrite($fp, hex2bin("4"));
 				break;
 			case '5':	
-				fwrite($fp, hex2bin("0101"));
+				fwrite($fp, hex2bin("5"));
 				break;
 			case '6':	
-				fwrite($fp, hex2bin("0110"));
+				fwrite($fp, hex2bin("6"));
 				break;
 			case '7':	
-				fwrite($fp, hex2bin("0111"));
+				fwrite($fp, hex2bin("7"));
 				break;
 			case '8':	
-				fwrite($fp, hex2bin("1000"));
+				fwrite($fp, hex2bin("8"));
 				break;
 			case '9':	
-				fwrite($fp, hex2bin("1001"));
+				fwrite($fp, hex2bin("9"));
 				break;
 			case 'A':	
 			case 'a':	
-				fwrite($fp, hex2bin("1010"));
+				fwrite($fp, hex2bin("a"));
 				break;
 			case 'B':	
 			case 'b':	
-				fwrite($fp, hex2bin("1011"));
+				fwrite($fp, hex2bin("b"));
 				break;
 			case 'C':	
 			case 'c':	
-				fwrite($fp, hex2bin("1100"));
+				fwrite($fp, hex2bin("c"));
 				break;
 			case 'D':	
 			case 'd':	
-				fwrite($fp, hex2bin("1101"));
+				fwrite($fp, hex2bin("d"));
 				break;
 			case 'E':	
 			case 'e':	
-				fwrite($fp, hex2bin("1110"));
+				fwrite($fp, hex2bin("e"));
 				break;
 			case 'F':	
 			case 'f':	
-				fwrite($fp, hex2bin("1111"));
+				fwrite($fp, hex2bin("f"));
 				break;
 			default:
 				/* BFIX truncate file */	
